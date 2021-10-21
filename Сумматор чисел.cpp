d5081d7ea6fa75a0cdd8efb91ca64b15f6f623f8
7roman0vna7 <90880098+7roman0vna7@users.noreply.github.com>
2021-10-22 01:51:35 +0300

    Add files via upload

---
+++ Сумматор чисел.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");     // Для определения русского языка
+	double a, b;
+	cout << "Введите первое произвольное число: ";
+	cin >> a;
+	cout << "Спасибо, Вы ввели: " << a << endl;
+	cout << "Введите второе произвольное число: ";
+	cin >> b;
+	cout << "Спасибо, Вы ввели: " << b << endl;
+	double c = 0;
+	c = a + b;
+	cout << "Вы получили: " << c << endl;
+	return 0;
+}
